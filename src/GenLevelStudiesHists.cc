2f1c07ab64cab2ee9f8e1588fabcda261448f829
Andrea Malara <andrea.malara@cern.ch>
2022-02-11 15:10:30 +0100

    revert hist booking

--- src/GenLevelStudiesHists.cc
+++ src/GenLevelStudiesHists.cc
@@ -20,29 +20,48 @@ using namespace std;
 
 GenLevelStudiesHists::GenLevelStudiesHists(TString dir_) : BaseHists(dir_){
 
-  book_1D<TH1F>("sumweights",      ";sum of event weights; Events / bin",      1,  0.5, 1.5);
-  book_1D<TH1F>("genmet",          ";gen. p_{T}^{miss} [GeV]; Events / bin", 100,    0, 1000);
-  book_1D<TH1F>("genmet_phi",      ";#phi(gen. p_{T}^{miss}); Events / bin",  60, -3.5, 3.5);
+  book<TH1F>("sumweights",      ";sum of event weights; Events / bin",      1,  0.5, 1.5);
 
-  book_1D<TH1F>("geninfoid1",      ";ID of initial state 1; Events / bin",    11, -5.5, 5.5);
-  book_1D<TH1F>("geninfoid2",      ";ID of initial state 2; Events / bin",    11, -5.5, 5.5);
-  book_1D<TH1F>("geninfox1",       ";x of initial state 1; Events / bin",     50,    0, 1);
-  book_1D<TH1F>("geninfox2",       ";x of initial state 2; Events / bin",     50,    0, 1);
-  book_1D<TH1F>("geninfoscalepdf", ";PDF scale; Events / bin",               200,    0, 3000);
+  book<TH1F>("geninfoscalepdf", ";PDF scale; Events / bin",               200,    0, 3000);
+  book<TH1F>("geninfoid1",      ";ID of initial state 1; Events / bin",    27, -5.5, 21.5);
+  book<TH1F>("geninfoid2",      ";ID of initial state 2; Events / bin",    27, -5.5, 21.5);
+  book<TH1F>("geninfox1",       ";x of initial state 1; Events / bin",     50,    0, 1);
+  book<TH1F>("geninfox2",       ";x of initial state 2; Events / bin",     50,    0, 1);
+  book<TH1F>("geninfoxg",       ";x of all gluons; Events / bin",          50,    0, 1);
+  book<TH1F>("geninfoxlight",   ";x of all light quarks; Events / bin",    50,    0, 1);
+
+  book<TH1F>("genmet",          ";gen. p_{T}^{miss} [GeV]; Events / bin", 100,    0, 1000);
+  book<TH1F>("genmet_phi",      ";#phi(gen. p_{T}^{miss}); Events / bin",  60, -3.5, 3.5);
+  book<TH2D>("genmet_phi_pt",   ";metphi;pt; Events / bin", 60, -3.5, 3.5, 100,    0, 1000);
 
 }
 
 void GenLevelStudiesHists::fill(const RecoEvent & event){
   double weight = event.weight;
-  fill_1D("sumweights", 1, weight);
+  int id1 = event.geninfo->id1();
+  int id2 = event.geninfo->id1();
+  hist<TH1F>("sumweights")->Fill(1, weight);
 
-  fill_1D("genmet",     event.genmet->pt(), weight);
-  fill_1D("genmet_phi", event.genmet->phi(), weight);
+  hist<TH1F>("geninfoscalepdf")->Fill(event.geninfo->scale_pdf(), weight);
+  hist<TH1F>("geninfoid1")->Fill(id1, weight);
+  hist<TH1F>("geninfoid2")->Fill(id2, weight);
+  hist<TH1F>("geninfox1")->Fill(event.geninfo->x1(), weight);
+  hist<TH1F>("geninfox2")->Fill(event.geninfo->x2(), weight);
 
-  fill_1D("geninfoid1",      event.geninfo->id1(), weight);
-  fill_1D("geninfoid2",      event.geninfo->id2(), weight);
-  fill_1D("geninfox1",       event.geninfo->x1(), weight);
-  fill_1D("geninfox2",       event.geninfo->x2(), weight);
-  fill_1D("geninfoscalepdf", event.geninfo->scale_pdf(), weight);
+
+  if(abs(id1) == 21) {
+    hist<TH1F>("geninfoxg")->Fill(event.geninfo->x1(), weight);
+    hist<TH1F>("geninfoxg")->Fill(event.geninfo->x2(), weight);
+  }
+  if(abs(id1) >= 1 && abs(id1) <= 3) {
+    hist<TH1F>("geninfoxlight")->Fill(event.geninfo->x1(), weight);
+    hist<TH1F>("geninfoxlight")->Fill(event.geninfo->x2(), weight);
+  }
+
+  hist<TH1F>("genmet")->Fill(event.genmet->pt(), weight);
+  hist<TH1F>("genmet_phi")->Fill(event.genmet->phi(), weight);
+  hist<TH2D>("genmet_phi_pt")->Fill(event.genmet->phi(), event.genmet->pt(), weight);
+
+
 
 }
