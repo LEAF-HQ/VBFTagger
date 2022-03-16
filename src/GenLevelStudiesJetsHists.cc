cd9b22221252843a44a7301d4ab2c42f08b541ba
Damien Delattre <d.damien97@gmail.com>
2022-03-16 22:27:24 +0100

    Update on jets observables

--- src/GenLevelStudiesJetsHists.cc
+++ src/GenLevelStudiesJetsHists.cc
@@ -29,17 +29,17 @@ GenLevelStudiesJetsHists::GenLevelStudiesJetsHists(TString dir_) : BaseHists(dir
   book<TH1F>("sumweights",      ";sum of event weights; Events / bin",      1,  0.5, 1.5);
   for (TString na : name) {
     for (double tresh : threshold) {
-      book<TH1F>(Title + "ngenjets" + tresh + na, Axis + ";N_{genjets} w/ Pt > " + tresh + "GeV" + na + "; Events / bin", 26, -0.5, 25.5);
+      book<TH1F>(Title + "ngenjets" + tresh + na, Axis + ";N_{genjets} w/ P_{t} > " + tresh + "GeV" + na + "; Events / bin", 21, -0.5, 20.5);
     }
-    book<TH1F>(Title + "jetsPT" + na, ";P_t"+na+";Number of jets", 251, -0.5, 250.5);
-    book<TH1F>(Title + "jetsEta" + na, ";#Eta"+na+"; Number of jets", 100, -7, 7);
-    book<TH1F>(Title + "jetsPhi" + na, ";#Phi"+na+"; Number of jets", 50, -4, 4);
-    book<TH1F>(Title + "jetsMass" + na, ";Mass (GeV)"+na+"; Number of jets", 51, -0.5, 50.5);
-    book<TH1F>(Title + "jetsdR" + na, ";#DeltaR (genjets, genparticles)"+na+";Events / bin", 100, 0, 5);
-    book<TH1F>(Title + "jetsHt" + na, ";#H_t"+na+";Events / bin", 1001, -0.5, 1000.5);
-    book<TH1F>(Title + "deltaEtaLeadingJets" + na, ";|#Delta #Eta|_{leading jets}"+na+";Events / bin", 1001, -0.5, 1000.5);
+    book<TH1F>(Title + "jetsPT" + na, ";P_{t}"+na+";Number of jets", 201, -0.5, 200.5);
+    book<TH1F>(Title + "jetsEta" + na, ";#eta"+na+"; Number of jets", 100, -7, 7);
+    book<TH1F>(Title + "jetsPhi" + na, ";#phi"+na+"; Number of jets", 50, -4, 4);
+    book<TH1F>(Title + "jetsMass" + na, ";Mass (GeV)"+na+"; Number of jets", 41, -0.5, 40.5);
+    book<TH1F>(Title + "jetsdR" + na, ";#DeltaR (genjets, genparticles)"+na+";Events / bin", 100, 0, 10);
+    book<TH1F>(Title + "jetsHt" + na, ";H_{t}"+na+";Events / bin", 401, -0.5, 400.5);
+    book<TH1F>(Title + "deltaEtaLeadingJets" + na, ";|#Delta #eta|_{leading jets}"+na+";Events / bin", 11, -0.5, 10.5);
+    book<TH1F>(Title + "dijetsMass" + na, "; 2 leading jets mass (GeV)"+na+"; Events", 301, -0.5, 300.5);
   }
-  book<TH1F>("dijetsMass", "; Dijets mass (GeV); Events", 1001, -0.5, 1000.5);
   }
 
 void GenLevelStudiesJetsHists::fill(const RecoEvent & event){
@@ -61,16 +61,19 @@ void GenLevelStudiesJetsHists::fill(const RecoEvent & event){
   double ht = 0.;
   double htLeptonFilter = 0.;
 
-  GenJet j1 = event.genjets->at(0);
-  GenJet j2 = event.genjets->at(1);
-  double dEta = deltaEta(j1, j2);
-  TLorentzVector v = j1.p4() + j2.p4();
-  hist<TH1F>(Title + "deltaEtaLeadingJets")->Fill(dEta, weight);
-  hist<TH1F>("dijetsMass")->Fill(v.M(), weight);
+  if (event.genjets->size() > 1) {
+    GenJet j1 = event.genjets->at(0);
+    GenJet j2 = event.genjets->at(1);
+    double dEta = deltaEta(j1, j2);
+    TLorentzVector v = j1.p4() + j2.p4();
+    hist<TH1F>(Title + "deltaEtaLeadingJets")->Fill(dEta, weight);
+    hist<TH1F>("dijetsMass")->Fill(v.M(), weight);
+  }
 
   // looping over all jets from the event
   for(size_t i=0; i<event.genjets->size(); i++){
     GenJet j = event.genjets->at(i);
+    cout <<"loop over jets "<<"size: "<<event.genjets->size()<< "   i: "<<i <<endl;
 
     ht += j.pt();
 
@@ -80,6 +83,7 @@ void GenLevelStudiesJetsHists::fill(const RecoEvent & event){
         treshsize[u] += 1;
       }
     }
+
     // filling basic histograms
     hist<TH1F>(Title + "jetsPT")->Fill(j.pt(), weight);
     hist<TH1F>(Title + "jetsEta")->Fill(j.eta(), weight);
@@ -87,7 +91,8 @@ void GenLevelStudiesJetsHists::fill(const RecoEvent & event){
     hist<TH1F>(Title + "jetsMass")->Fill(j.m(), weight);
 
     // loop over all the gen particles to remove leptons from the jets using dR < 0.4
-    int leptonFlag = 0;
+    int leptonFlagJet1 = 0;
+    int leptonFlagJet2 = 0;
     for (size_t k=0; k<event.genparticles_all->size(); k++) {
       GenParticle gp = event.genparticles_all->at(k);
 
@@ -96,25 +101,39 @@ void GenLevelStudiesJetsHists::fill(const RecoEvent & event){
       double dR = deltaR(j, gp);
       hist<TH1F>(Title + "jetsdR")->Fill(dR, weight);
 
-      if (abs(gp.pdgid()) <= 11 || abs(gp.pdgid()) >= 18) {
-          double dR_filter = deltaR(j, gp);
-          if (dR_filter < 0.4) {
-            leptonFlag = 1;
-            break;
+      if (abs(gp.pdgid()) >= 11 && abs(gp.pdgid()) <= 18) {
+          if (dR < 0.4) {
+            leptonFlagJet1 = 1;
+          }
+          if (i == 0 && event.genjets->size() > 1) {
+            GenJet jf = event.genjets->at(i+1);
+            double dR_filter2 = deltaR(jf, gp);
+            if (dR_filter2 < 0.4) {
+              leptonFlagJet2 = 1;
+            }
           }
       }
     }
-    if (leptonFlag == 0) {
+    if (leptonFlagJet1 == 0) {
       for (size_t u=0; u<threshold.size(); u++) {
         if (j.pt() > threshold[u]) {
           treshsizeFilter[u] += 1;
         }
+      }
+      if (i == 0 && leptonFlagJet2 == 0 && event.genjets->size()>1) {
+        GenJet jf1 = event.genjets->at(i);
+        GenJet jf2 = event.genjets->at(i+1);
+        double dEtaf = deltaEta(jf1, jf2);
+        TLorentzVector vf = jf1.p4() + jf2.p4();
+        hist<TH1F>(Title + "deltaEtaLeadingJets& lepton filter")->Fill(dEtaf, weight);
+        hist<TH1F>("dijetsMass& lepton filter")->Fill(vf.M(), weight);
       }
       htLeptonFilter += j.pt();
       hist<TH1F>(Title + "jetsPT& lepton filter")->Fill(j.pt(), weight);
       hist<TH1F>(Title + "jetsEta& lepton filter")->Fill(j.eta(), weight);
       hist<TH1F>(Title + "jetsPhi& lepton filter")->Fill(j.phi(), weight);
       hist<TH1F>(Title + "jetsMass& lepton filter")->Fill(j.m(), weight);
+      hist<TH1F>(Title + "jetsdR& lepton filter")->Fill(dR, weight);
     }
   }
   for (size_t u=0; u<threshold.size(); u++) {
